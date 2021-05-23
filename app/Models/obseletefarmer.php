59869a8aab28a9b2d467933257bea5b70f06f794
Hamid-Zahoor <80816925+Hamid-Zahoor@users.noreply.github.com>
2021-05-23 22:54:16 +1000

    Update and rename farmer.php to obseletefarmer.php

--- app/Models/obseletefarmer.php
+++ app/Models/obseletefarmer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class farmer extends Model
+class obseletefarmer extends Model
 {
     use HasFactory;
     public $timestamps=false;
